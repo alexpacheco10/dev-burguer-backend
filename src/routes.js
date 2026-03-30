1757f93593bb49f3cacd06aab2fc419a21d2cdd6
alexpacheco10 <alexoficial.pacheco@gmail.com>
2026-03-30 18:34:52 -0400

    criando categorias

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import ProductController from './app/controllers/ProductController.js';
 
 import multerConfig from './config/multer.cjs';
 import authMiddleware from './middlewares/auth.js';
+import CategoryController from './app/controllers/CategoryController.js';
 
 const routes = new Router();
 
@@ -17,6 +18,9 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index );
+
+routes.post('/categories', CategoryController.store);
+routes.get('/categories', CategoryController.index );
 
 
 export default routes;
